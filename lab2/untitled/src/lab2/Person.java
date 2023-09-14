b265910d6dbccd903668be20160839c2bf9ed6c7
DmytroProg <dmytroprog@gmail.com>
2023-09-14 12:45:33 +0300

    class Person added

---
+++ lab2/untitled/src/lab2/Person.java
@@ -0,0 +1,77 @@
+package lab2;
+
+public class Person {
+    private String name;
+    private String surname;
+    private String middleName;
+    private String address;
+    private String phoneNumber;
+
+    public Person(String name, String surname, String middleName, String address, String phoneNumber){
+        setName(name);
+        setSurname(surname);
+        setMiddleName(middleName);
+        setAddress(address);
+        setPhoneNumber(phoneNumber);
+    }
+
+    public void setName(String name){
+        if(name.isEmpty()) {
+           throw new IllegalArgumentException("name must not be empty");
+        }
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setSurname(String surname){
+        if(surname.isEmpty()) {
+            throw new IllegalArgumentException("surname must not be empty");
+        }
+        this.surname = surname;
+    }
+
+    public String getSurname(){
+        return surname;
+    }
+
+    public void setMiddleName(String middleName){
+        if(middleName.isEmpty()) {
+            throw new IllegalArgumentException("middle name must not be empty");
+        }
+        this.middleName = middleName;
+    }
+
+    public String getMiddleName(){
+        return middleName;
+    }
+
+    public void setAddress(String address){
+        if(address.isEmpty()) {
+            throw new IllegalArgumentException("address must not be empty");
+        }
+        this.address = address;
+    }
+
+    public String getAddress(){
+        return address;
+    }
+
+    public void setPhoneNumber(String phoneNumber){
+        if(phoneNumber.isEmpty()) {
+            throw new IllegalArgumentException("phone number must not be empty");
+        }
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPhoneNumber(){
+        return phoneNumber;
+    }
+
+    @Override
+    public String toString(){
+        return surname + " " + name + " " + middleName + ", " + address + " phone: (" + phoneNumber + ")";
+    }
+}
